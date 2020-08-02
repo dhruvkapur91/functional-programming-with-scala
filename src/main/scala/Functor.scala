f0b313fd4ca9ed029d730bef880397b3b080d1d8
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-02 22:03:23 +0530

    Add. Composition law for functor

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -8,6 +8,10 @@ trait FunctorLaws {
   def identity[F[_],A](fa : F[A])(implicit F : Functor[F]) = {
     F.map(fa)(x => x) == fa
   }
+
+  def composition[F[_],A,B,C](fa : F[A], f : A=>B, g : B=>C)(implicit  F : Functor[F]) = {
+    F.map(F.map(fa)(f))(g) == F.map(fa)(f andThen g)
+  }
 }
 
 object Main extends App {
