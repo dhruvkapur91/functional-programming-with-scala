83fbfc5ec241c04e53ea7b86938744f3032d5346
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-02 23:16:07 +0530

    Add. as method for functor

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -4,6 +4,8 @@ trait Functor[F[_]] {
   def map[A,B](fa : F[A])(f : A => B) : F[B]
 
   def lift[A,B](f : A => B) : F[A] => F[B] = map(_)(f)
+
+  def as[A,B]( fa : F[A], b : => B) : F[B] = lift((_: A) => b)(fa)
 }
 
 object Functor {
