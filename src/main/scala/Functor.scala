15a6e891053b961ffe29c115300e701226875d00
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-08-02 19:11:42 +0530

    Add. Identity functor law

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -1,3 +1,15 @@
+import simulacrum._
+
 trait Functor[F[_]] {
   def map[A,B](fa : F[A])(f : A => B) : F[B]
+}
+
+trait FunctorLaws {
+  def identity[F[_],A](fa : F[A])(implicit F : Functor[F]) = {
+    F.map(fa)(x => x) == fa
+  }
+}
+
+object Main extends App {
+  println("Everything compiles")
 }
